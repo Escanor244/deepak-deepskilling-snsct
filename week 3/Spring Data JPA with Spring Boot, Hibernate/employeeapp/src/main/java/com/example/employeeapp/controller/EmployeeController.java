64cecc0b59a879ec45a8e990c7ada31c1c538c5a
Escanor244 <162327328+Escanor244@users.noreply.github.com>
2025-07-06 20:14:43 +0530

    deepskilling

---
+++ week 3/Spring Data JPA with Spring Boot, Hibernate/employeeapp/src/main/java/com/example/employeeapp/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.example.employeeapp.controller;
+
+import com.example.employeeapp.model.Employee;
+import com.example.employeeapp.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+}
